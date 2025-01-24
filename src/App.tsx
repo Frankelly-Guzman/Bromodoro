9c048212cb77e3f78b186bbd1ddd3c803fb116cc
Rafael Niebles <rnieblesealo@gmail.com>
2025-01-24 14:29:20 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='min-h-screen min-w-screen bg-black text-white'>Hello, World!</div>
+    <div className='min-h-screen min-w-screen bg-black text-white'>Hello, Frontend!</div>
   )
 }
 
